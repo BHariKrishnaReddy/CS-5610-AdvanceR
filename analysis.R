bbb509d89ec5af85aac69eced21d4b33ab6672e7
BHariKrishnaReddy <reddyfrn007@gmail.com>
2024-01-16 19:10:23 -0500

    Modified analysis.R

---
+++ analysis.R
@@ -0,0 +1,19 @@
+# Create variables
+a <- 5
+b <- 3
+
+# Addition
+c <- a + b
+print(paste("Sum:", c))
+
+# Subtraction
+c <- a - b
+print(paste("Difference:", c))
+
+# Multiplication
+c <- a * b
+print(paste("Product:", c))
+
+# Division
+c <- a / b
+print(paste("Quotient:", c))
